d467ee8640477cab700e373812f02586c116dfc5
sergey <sbarchshevskiy@gmail.com>
2021-02-15 20:53:56 +0000

    added random string generator

--- express_server.js
+++ express_server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded(
+  {
+    extended : true,
+  }
+));
 
 app.set('view engine', 'ejs');
 
@@ -18,6 +25,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  // route to create a new URL, route has to stay above urls/:id
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  // : for url ID, templateVars linked to urls_show
+  const templateVars = { shortURL : req.params.shortURL,
+    longURL : req.params.longURL,
+    newURL : req.params.newURL
+  };
+  res.render("urls_show", templateVars);
+});
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -25,7 +47,25 @@ app.get("/", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Helloooo <b>World</b></body></html>\n");
+
 });
+
+app.post("/urls/new", (req, res) => {
+  req.body();
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+const generateRandomString = function() {
+  let randomStr = "";
+  const alphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++)
+    randomStr += alphaNum.charAt(Math.floor(Math.random() * alphaNum.length));
+  return randomStr;
+};
 
 
 app.listen(PORT, () => {
